a51a6622dd298522549c4203ab46b5ee71fe87eb
Abiyu Tegegn <ategegn6@gatech.edu>
2021-04-19 22:36:45 -0400

    added board and the squares.

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+    render(){
+        return(
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                { this.state.value }
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component { 
+    renderSquare(i){
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next Player: X';
+
+        return(
+            <div>
+                <div className='status'>{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                    {this.renderSquare(6)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                    {this.renderSquare(9)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render(){
+        return(
+            <div className="game">
+                <div className="game-baord"> 
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>
+                        {/* { status } */}
+                        <ol> { /* TODO */ } </ol>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+)
